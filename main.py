d0814e2db42a4b65c7d8a9dd828377c12c627699
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-10 14:19:29 -0600

    added dynamic scraping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 from selenium.webdriver.common.by import By
 
 """
@@ -8,6 +9,8 @@ start maximized means that the page we are scraping gets maximized, since someti
 disable dev shm is related to linux
 no sandbox gives our script greater privileges on pages
 the experimental options we added is to help our scripts not be detected by web pages
+
+analogy: think of the driver as a remote control for a web page, and the python script is the one pushing the controller buttons
 """
 def get_driver():
   options = webdriver.EdgeOptions()
@@ -18,13 +21,14 @@ def get_driver():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-features=AutomationControlled")
  
-  driver = webdriver.Edge(options=options)
-  driver.get("http://automated.pythonanywhere.com")
+  driver = webdriver.Edge(options=options)              #the driver is responsible for launching the browser and controlling it, and allows us to use methods like find_elements() etc.
+  driver.get("http://automated.pythonanywhere.com")     #loads the web page to prepare it for scraping
   return driver
  
 def main():
   driver = get_driver()
-  element = driver.find_element(By.XPATH, "/html/body/div[1]/div/h1[1]")
+  time.sleep(3)        #the dynamic value takes about 2-3 seconds to load onto the html page, so we do not want to scrape the page until after that value has loaded in.
+  element = driver.find_element(By.XPATH, value="/html/body/div[1]/div/h1[2]")      #driver.find_element() returns a WebElement object, which we are then assigning to our variable called 'element' (that's where the .text field comes from)
   return element.text
  
 print(main())
