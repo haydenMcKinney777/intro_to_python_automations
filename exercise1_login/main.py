782a6c4bae53d5ea81f290d36a920e758401599d
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-12 13:01:47 -0600

    completed beautifulsoup exercise

--- exercise1_login/main.py
+++ exercise1_login/main.py
@@ -1,38 +1,19 @@
 """
-STOCK DATA: Ask user for ticket symbol, start & end date, use that data to search for that specific stock
-            during the time period that the user gave and process the data into a new csv file.
-
-TO DO: figure out how to get a reliable URL. so far, I have not been able to find any way of copying the link to the 
-       "download" button on the stock's historical data
-
+BEAUTIFUL SOUP: beautiful soup is a library for web scraping, while selenium is a hybrid between
+                web scraping and browser automation. in this file i will learn how to use beautiful
+                soup to scrape web data from a currency exchange rate website.
 """
 
-import requests                                                                             #needed for GET request for our URL
-from datetime import datetime
-import time
+from bs4 import BeautifulSoup
+import requests
 
-ticker_symbol = input("Enter the ticker symbol for the stock data you would like to view: ")
-from_date = input("Enter the date to start from in yyyy-mm-dd: ")
-to_date = input("Enter the date to end at in yyyy-mm-dd: ")
+def get_currency(input_currency, output_currency):
+    url = f"https://www.x-rates.com/calculator/?from={input_currency}&to={output_currency}&amount=1"
+    content = requests.get(url).text                                                                 #requests.get(url) will return the status of the GET request. but if we add .text at the end, we get the source code for the html page, since that is obviously what would be replied with in a GET request
+    soup = BeautifulSoup(content, "html.parser")                                                     #we create an instance of BeautifulSoup called 'soup' which expects a page's markup (held in content) and we also tell it we want to use an html parser which will be used to parse through the markup code for the <span> element that holds the number for the exchange rate on the webpage.
+    rate = soup.find("span", class_="ccOutputRslt").get_text()                                       #the element on the webpage that holds the exchange rate is a span with a class of 'ccOutputRslt'. If we do not include .get_text() at the end, beautiful soup will return the markup for the span in which the currency rate number lives inside, as well as all child elements of the span class. Adding .get_text() ensures we only grab the exchange rate number with its related tag (USD, AUD, etc). Note that this value will be a string
+    currency = float(rate[0:8])                                                                      #this makes sure we only grab the number of the exchange rate, only grabbing the string between index 0 and 8. This value is still a string, so we convert it to a float in this same step as well.
 
-try:
-    from_datetime = datetime.strptime(from_date, '%Y-%m-%d')                                #validate date format
-    to_datetime = datetime.strptime(to_date, '%Y-%m-%d')
-except ValueError:
-    print("Invalid date format. Please enter dates in yyyy-mm-dd format.")
-    exit()
+    return currency
 
-from_date_formatted = from_datetime.strftime('%Y-%m-%d')
-to_date_formatted = to_datetime.strftime('%Y-%m-%d')
-
-# from_epoch = int(from_datetime.timestamp())
-# to_epoch = int(to_datetime.timestamp())
-
-url = f"https://api.nasdaq.com/api/quote/{ticker_symbol}/historical?assetclass=stocks&fromdate={from_date_formatted}&limit=9999&todate={to_date_formatted}&random=24"
-
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"} #these headers are used to basically disguise ourselves from the webpage we're accessing, making us look like a regular browser
-
-content = requests.get(url, headers=headers).content                                        #this is the content that the GET request will return
-
-with open("data.csv", "wb") as file:                                                        #note that 'wb' is a more general write option. it is able to deal with both byte-data and non byte-data. if we were to only use "w" we could only work with text data, which would also mean we would have to use .text instead of .content on the line above
-    file.write(content)
+print(get_currency('INR', 'AUD'))
