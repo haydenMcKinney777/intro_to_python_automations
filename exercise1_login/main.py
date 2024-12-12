1a04635ee1e41a448d4825da07c979fa78c4cdcb
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-12 15:44:48 -0600

    added functionality for dynamic links based on user input

--- exercise1_login/main.py
+++ exercise1_login/main.py
@@ -8,15 +8,21 @@ NEWSAPI: this file shows how we can simply use news API to gather information fr
 
 import requests
 
-r = requests.get("https://newsapi.org/v2/everything?qInTitle=Black%20Ops%206&apiKey=50ae6f8dd35c4e86bd73945f2b845548")      #GET requests return data in JSON format
+# r = requests.get("https://newsapi.org/v2/everything?qInTitle=Black%20Ops%206&apiKey=50ae6f8dd35c4e86bd73945f2b845548")      #GET requests return data in JSON format
+# print(r.headers['Content-Type'])                                                                                            #simply print statement to verify that our GET request returns data in JSON format
+# content = r.json()                                                                                                          #turns the json data given by our get request into a python dictionary full of the json data
+# articles = content['articles']                                                                                              #here we are accessing the dictionary that we created and we are grouping all of the articles into a variable. articles is a dictionary itself, which contains more lists and dictionaries. with this, we are able to access all of the information that we want to from the news stories that the API gathers, such as the titles of each articles, descriptions of them, etc. 
+# for article in articles:
+#     print("\nTITLE: ", article['title'], "\nDESCRIPTION:\n", article['description'], "\nPUBLISHED AT:\n", article['publishedAt'])
 
-print(r.headers['Content-Type'])                                                                                        #simply print statement to verify that our GET request returns data in JSON format
+def get_news(topic, from_date, to_date, language='en', api_key='50ae6f8dd35c4e86bd73945f2b845548'):
+    url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language={language}&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json()
+    articles = content['articles']
+    results = []
+    for article in articles:
+        results.append(f"\nTITLE: {article['title']}\nDESCRIPTION: {article['description']}")
+    return results
 
-content = r.json()                                                                                                      #turns the json data given by our get request into a python dictionary full of the json data
-
-articles = content['articles']                                                                                          #here we are accessing the dictionary that we created and we are grouping all of the articles into a variable. articles is a dictionary itself, which contains more lists and dictionaries. with this, we are able to access all of the information that we want to from the news stories that the API gathers, such as the titles of each articles, descriptions of them, etc. 
-
-for article in articles:
-    print("\nTITLE: ", article['title'], "\nDESCRIPTION:\n", article['description'], "\nPUBLISHED AT:\n", article['publishedAt'])
-
-print(articles)
+print("\n".join(get_news(topic='space', from_date='2024-11-20', to_date='2022-12-12')))
