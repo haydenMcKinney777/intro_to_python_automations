6fd8aafa68256b36d1534cc24ded5d8385ebdf8c
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-14 14:01:19 -0600

    added basic functionality to grab weather data

--- exercise1_login/main.py
+++ exercise1_login/main.py
@@ -4,11 +4,15 @@ WEATHER FORECAST:
 
 import requests
 
-def get_weather(city, api_key):
+def get_weather(city, api_key="1fa676ddb6c81cca2ef15f0c56cb2ebc"):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}"
     response = requests.get(url)
-    print(response)
     content = response.json()
-    print(content)
+    weather_types = content['weather']
+    weather_summary = []
+    for weather in weather_types:
+        weather_summary.append(weather)
+    return weather_summary
 
-get_weather("Waxahachie", "1fa676ddb6c81cca2ef15f0c56cb2ebc")
+weather_summary = get_weather("Waxahachie")
+print(weather_summary, "\n")
