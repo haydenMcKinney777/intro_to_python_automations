24ea5cc126f1e112386d2a2fb214c7c1c609b1cf
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-14 14:33:43 -0600

    cleaned up code

--- weatherforecast/main.py
+++ weatherforecast/main.py
@@ -10,17 +10,12 @@ def get_weather(city, api_key="1fa676ddb6c81cca2ef15f0c56cb2ebc"):
     content = response.json()
     weather_types = content["list"]
     city_name = content["city"]["name"]
-    weather_summary = []
-    # for weather in weather_types:
-    #     weather_summary.append(weather)
-    # return weather_summary
-
     with open("./data.txt", "a") as file:
         for weather_list in weather_types:
             temperature = weather_list["main"]["temp"]
             datetime_text = weather_list["dt_txt"]
             condition = weather_list["weather"][0]["description"]
-            file.write(f"{city_name},{datetime_text},{temperature},{condition}\n")    
+            file.write(f"{city_name}\t\t{datetime_text}\t\t{temperature}\t\t{condition}\n")    
     print(f"Weather data for {city_name} appended to 'data.txt'.")
 
 get_weather("Waxahachie")
