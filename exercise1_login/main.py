3ce25143d16cc526cd129cc7ea66ed636140464f
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-14 14:29:55 -0600

    finished example of grabbing weatherforecast data

--- exercise1_login/main.py
+++ exercise1_login/main.py
@@ -8,11 +8,19 @@ def get_weather(city, api_key="1fa676ddb6c81cca2ef15f0c56cb2ebc"):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}"
     response = requests.get(url)
     content = response.json()
-    weather_types = content['weather']
+    weather_types = content["list"]
+    city_name = content["city"]["name"]
     weather_summary = []
-    for weather in weather_types:
-        weather_summary.append(weather)
-    return weather_summary
+    # for weather in weather_types:
+    #     weather_summary.append(weather)
+    # return weather_summary
 
-weather_summary = get_weather("Waxahachie")
-print(weather_summary, "\n")
+    with open("./data.txt", "a") as file:
+        for weather_list in weather_types:
+            temperature = weather_list["main"]["temp"]
+            datetime_text = weather_list["dt_txt"]
+            condition = weather_list["weather"][0]["description"]
+            file.write(f"{city_name},{datetime_text},{temperature},{condition}\n")    
+    print(f"Weather data for {city_name} appended to 'data.txt'.")
+
+get_weather("Waxahachie")
