af63d47b812ae6d15b12d146b82a8b44592ded80
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-16 17:44:57 -0600

    added starting code for flask

--- weatherforecast/main.py
+++ weatherforecast/main.py
@@ -1,29 +1,9 @@
-"""
-WEATHER FORECAST: here we use weatherforecastAPI to grab weather data from a city that the user
-                  will provide to the program. for documentation or to view the JSON format used
-                  by the api, visit https://openweathermap.org/forecast5
+from flask import Flask
 
-                  This code will grab weather data from the provided city, and append it to a .txt
-                  file "data.txt" which has column for city, time, temperature, and condition.
-                  Note that the data.txt file has a newline character after the first and only written
-                  line in the file. This is there so that when data gets appended to the file, it will
-                  be properly formatted.
-"""
+app = Flask(__name__)
 
-import requests
+@app.route('/')
+def home():
+    return '<h1>Currency Rate API</h1> <p>Example URL: /api/v1/usd-eur</p>'
 
-def get_weather(city, units='imperial', api_key="1fa676ddb6c81cca2ef15f0c56cb2ebc"):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&units={units}&appid={api_key}"           #more data can be accessed by modifying this url
-    response = requests.get(url)
-    content = response.json()
-    weather_types = content["list"]
-    city_name = content["city"]["name"]
-    with open("./data.txt", "a") as file:
-        for weather_list in weather_types:                                                      #weather_types is everything inside the 'list' dictionary within the API
-            temperature = weather_list["main"]["temp"]
-            datetime_text = weather_list["dt_txt"]
-            condition = weather_list["weather"][0]["description"]
-            file.write(f"{city_name}\t\t{datetime_text}\t\t{temperature}\t\t{condition}\n")    
-    print(f"Weather data for {city_name} appended to 'data.txt'.")
-
-get_weather("Waxahachie")
+app.run()
