d19f65f9353693b27fd8e20496982ce412034ae9
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-18 15:49:12 -0600

    updated flask code, ready to insert beautiful soup

--- weatherforecast/main.py
+++ weatherforecast/main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)       #__name__ holds the string of the current module
 
 @app.route('/')
 def home():
-    return '<h1>Currency Rate API</h1> <p>Example URL: /api/v1/usd-eur</p>'
+    return f'<h1>Currency Rate API</h1> <p>Example URL: /api/v1/usd-eur</p>'
+
+@app.route('/api/v1/<input_currency>-<output_currency>')
+def api(input_currency, output_currency):
+    
 
 app.run()
