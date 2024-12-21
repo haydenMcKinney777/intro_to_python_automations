03b6e01592f5a48eb57a7acf0aba20ce3686a4ce
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-21 15:59:48 -0600

    completed exercise

--- renamefiles/main.py
+++ renamefiles/main.py
@@ -1,16 +1,18 @@
 """
-Exercise2: Rename all files based on "sub-sub-folders"
+Exercise3: add created date to all filenames in folder
 
 """
 
 from pathlib import Path
+from datetime import datetime
 
-root_directory = Path('files')
+root_directory = Path("files")
 
-for path in root_directory.glob('**/*'):
-    if path.is_file():                                                                          #we only want files. .glob() will list every single directory, we don't want that.
-        subfolders = path.parts[1:-1]           #from the first index (which is the first subfolder inside the "files" folder) to the -1 index which is the file.
-        print(subfolders)
-        new_file_name = "-".join(subfolders) + "-" + path.name      #.joins() method will take 'subfolders' and separate each thing in the 'subfolders' tuple by a '-'. this makes it so that any subfolder path no matter how deep will correctly be separated with a dash.
-        new_path_name = path.with_name(new_file_name)               #this line is needed because if we were to just say path.rename(new_file_name) the renamed files would not be placed in their original folders.
-        path.rename(new_path_name)
+for path in root_directory.glob("**/*"):
+    if path.is_file():
+        created_time_seconds = datetime.fromtimestamp(path.stat().st_birthtime)          #path.stat() gives us statistics about the file, such as when it was created. st_birthtime is that time (in timestamp format, not string)
+        created_time_date = created_time_seconds.strftime("%Y-%m-%d_%H-%M-%S")           #in order to append the date and time to our file names it needs to be converted into a string using .strftime()
+        path_parts = path.parts[1:]
+        new_file_name = created_time_date + "-" + ("-".join(path_parts))
+        new_file_path = path.with_name(new_file_name)
+        path.rename(new_file_path)
