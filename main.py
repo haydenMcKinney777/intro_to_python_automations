8c3baa54ef8e5e08417412d67f9c3023481841c4
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-22 14:50:08 -0600

    finished exercise and added functionality for file checking

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 """
 Exercise5: search for files on your computer given a search term
+
+this exercise is set up in a way that there are multiple .txt files with '14' found in them.
 """
 from pathlib import Path
 root_directory = Path('files')
 
-for path in root_directory.glob("**/*"):
-    for i in range(10,21):
-        destination_path = root_directory / Path(f"{i}.txt")
-        path.
+search_term = input("Enter search term for file: ")
 
+for path in root_directory.rglob('*.txt'):
+    if path.is_file():
+        if search_term in path.stem:
+            print(path.absolute())
 
-# search_term = input("Enter search term for file: ")
