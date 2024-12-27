4d435eb8a953c9fa01253f81f31c017f4de3b6df
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-27 14:48:00 -0600

    first commit

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-"""
-sending emails including attachments, such as .txt files
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-really, all you have to do is put the contents of the email into a list
+driver = webdriver.Edge()
+driver.get('https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6')
 
-"""
-
-import yagmail
-import os
-import pandas
-
-#accessing environment variables that i set up on my local machine to keep email and password safe.
-my_email = os.getenv('gmail_email')         
-my_password = os.getenv('app_password_gmail')
-sender = my_email
-subject = 'Automated email test'
-df = pandas.read_csv('contacts.csv')
-yag = yagmail.SMTP(user=sender, password=my_password)
-
-def generate_file(filename, content):
-    with open(filename, 'w') as file:
-        file.write(content)
-
-for index, row in df.iterrows():
-    name = row['name']
-    amount = str(row['amount'])     #this has to be a string in order for it to be written to our check.txt file
-    receiver = row['email']
-    generate_file(name, amount)
-    
-    email_body = [f"""
-    Hi {name}! You must pay your bill, which is:
-    {amount}.\nThank you.
-    """, 'check.txt']                    #notice here we add check.txt to the list
-    
-    yag.send(to=receiver, subject=subject, contents=email_body)
-
-print("Email sent successfully.\n")
+stock_trend = driver.find_element(By.CSS_SELECTOR, ".stock-trend.trend-drop")
+print(type(stock_trend))
