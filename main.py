a6c501ab692c9c2bc4b6fffbc58549ff461896f2
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-27 11:54:16 -0600

    updated email contents to be a list to send with attachement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 sending emails including attachments, such as .txt files
 
+really, all you have to do is put the contents of the email into a list
+
 """
 
 import yagmail
@@ -17,12 +19,12 @@ yag = yagmail.SMTP(user=sender, password=my_password)
 
 df = pandas.read_csv('contacts.csv')    #we use pandas to help us analyze our data in the csv file. 'df' = "dataframe"
 for index, row in df.iterrows():
-    email_body = f"""
+    email_body = [f"""
     Hi {row['name']}! This email has been automated using python.
     
     Thanks for reading! 
     
     -Python
-    """
+    """, 'text.txt']                    #notice here we add text.txt to the list
     yag.send(to=row['email'], subject=subject, contents=email_body)
 print("Email sent successfully.\n")
