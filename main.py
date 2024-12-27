acb85a102c2a66460dab73a2d558e836d25d7bcc
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-27 11:56:29 -0600

    removed pandas library - not needed

--- main.py
+++ main.py
@@ -7,24 +7,23 @@ really, all you have to do is put the contents of the email into a list
 
 import yagmail
 import os
-import pandas
 
 #accessing environment variables that i set up on my local machine to keep email and password safe.
 my_email = os.getenv('gmail_email')         
 my_password = os.getenv('app_password_gmail')
 
 sender = my_email
+receiver = os.getenv('email')
 subject = 'Automated email test'
 yag = yagmail.SMTP(user=sender, password=my_password)
 
-df = pandas.read_csv('contacts.csv')    #we use pandas to help us analyze our data in the csv file. 'df' = "dataframe"
-for index, row in df.iterrows():
-    email_body = [f"""
-    Hi {row['name']}! This email has been automated using python.
-    
-    Thanks for reading! 
-    
-    -Python
-    """, 'text.txt']                    #notice here we add text.txt to the list
-    yag.send(to=row['email'], subject=subject, contents=email_body)
+email_body = [f"""
+Hi! This email has been automated using python.
+
+Thanks for reading! 
+
+-Python
+""", 'text.txt']                    #notice here we add text.txt to the list
+yag.send(to=row['email'], subject=subject, contents=email_body)
+
 print("Email sent successfully.\n")
