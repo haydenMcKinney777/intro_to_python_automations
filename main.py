253a84c3c512a0d202449807639422248a34477b
haydenMcKinney777 <hemc7@sbcglobal.net>
2024-12-30 11:44:18 -0600

    finished flask code

--- main.py
+++ main.py
@@ -1,48 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait     #this replaces time.sleep. using webdriverwait is more time efficient
-from selenium.webdriver.support import expected_conditions as EC
-import yagmail
-import os
-import re      #for cleaning up text
+"""
 
-def get_driver():
-  options = webdriver.EdgeOptions()
-  options.add_argument("disable-infobars")
-  options.add_argument("start-maximized")
-  options.add_argument("disable-dev-shm-usage")
-  options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches", ["enable-automation"])
-  options.add_argument("disable-blink-features=AutomationControlled")
-  options.add_argument("log-level=3")
- 
-  driver = webdriver.Edge(options=options)              #the driver is responsible for launching the browser and controlling it, and allows us to use methods like find_elements() etc.
-  driver.get("https://zse.hr/en/indeks-366/365?isin=HRZB00ICBEX6")     #loads the web page to prepare it for scraping
-  return driver
+docs: https://tedboy.github.io/flask/generated/generated/flask.Request.html
+"""
 
+from flask import Flask, render_template, request
 
-def send_email(percent):
-    sender = os.getenv('gmail_email')
-    password = os.getenv('app_password_gmail')
-    receiver = os.getenv('email')
-    yag = yagmail.SMTP(sender, password)
+app = Flask(__name__)                                      #initialize flask
 
-    subject = "Stock Percentage Dropped Below -0.10%!"
-    contents = f"This email is to inform you that the stock price is now {percent}%!\n"
+@app.route('/')                                            #when the user visits the homepage, we want function home() to run. this is a standard GET request
+def home():
+    print("GET request")
+    return render_template('index.html')                   #flask will go into our 'templates' folder, and render index.html. This is why a templates folder is always required.
 
-    yag.send(to=receiver, subject=subject, contents=contents)
-    print("Email has been successfully sent.\n")
-
-
-def main():
-    driver = get_driver()
-    try:
-        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".stock-trend.trend-drop")))
-
-        stock_percentage = driver.find_element(By.XPATH, '//*[@id="app_indeks"]/section[1]/div/div/div[2]/span[2]').text
-        cleaned_stock_percentage = float(re.sub(r"[^\d.-]", "", stock_percentage))     #\d = any decimal, . = literal dot, - = literal minus, ^ in square brackets is boolean NOT, so we're saying anything that is not a digit, dot, or minus sign, take it out
-        send_email(cleaned_stock_percentage)
-    finally:
-        driver.quit()
-
-main()
+@app.route('/', methods = ['POST'])                        #here we are allowing our flask app to be able to handle POST requests, which we will need for when the user sends the data for the volume to be computed.                   
+def home_post():
+    dim1 = request.form.get('first_dimension')
+    dim2 = request.form.get('second_dimension')
+    dim3 = request.form.get('third_dimension')
+    volume = float(dim1) * float(dim2) * float(dim3)
+    print("POST request")
+    return render_template('index.html', output=volume, dim_1=dim1, dim_2=dim2, dim_3=dim3)    #renders the html page again but passes the calculated volume as a variable 'output' so that it can be displayed on the page. we also send dim1, dim2, dim3 again so that when we calculate the output, the parameters that the user entered do not disappear.
+app.run()                                                  #run our app on localhost
