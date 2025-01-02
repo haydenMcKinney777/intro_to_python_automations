bba333db8c51fff6e07f4da326c850ee584593fc
haydenMcKinney777 <hemc7@sbcglobal.net>
2025-01-02 14:39:25 -0600

    changed GUI layout setup and formatting

--- currency_converter.py
+++ currency_converter.py
@@ -5,9 +5,9 @@ and the gui with PyQT
 In this branch, we are giving the GUI a dropdown menu for the user to pick which currencies they would like to convert between
 """
 
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QHBoxLayout,
                                QVBoxLayout, QWidget, QLineEdit, QComboBox)
-
+from PySide6.QtCore import Qt
 from bs4 import BeautifulSoup
 import requests
 
@@ -21,7 +21,7 @@ def get_exchange_rates(input_currency='USD', output_currency='EUR'):
     return rate
 
 def convert():
-    input_text = textbox.text()                                     #we store the text that the user provided in the textbox to a variable called input_text
+    input_text = input_textbox.text()                                     #we store the text that the user provided in the textbox to a variable called input_text
     input_currency = input_combo.currentText()                      #we set the input currency to whatever the user selected in the first dropdown
     target_currency = target_combo.currentText()
     rate = get_exchange_rates(input_currency, target_currency)
@@ -33,32 +33,38 @@ app = QApplication([])                                              #creating an
 window = QWidget()                                                  #creating a window instance to hold all the elements of our application
 window.setWindowTitle('Currency Converter')
 
-#we add widgets (elements that are visible in the window) to something called a layout. here we are creating a textbox. Note that we have to manually attach the layout to the window - creating just a layout and adding widgets to it will not show up
 layout = QVBoxLayout()                                              #note 'QV' as in vertical. So if we add anymore widgets to this layout, they will be stacked vertically. could use QHLayout instead for horizontal
+
+layout_top = QHBoxLayout()
+layout.addLayout(layout_top)
+
+output_label = QLabel('')                                           #when the application first runs, we do not want anything to appear in the label so we just give it an empty string
+layout.addWidget(output_label)
+
+layout_left = QVBoxLayout()                                         #the droptown buttons will be here
+layout_top.addLayout(layout_left)
+
+layout_right = QVBoxLayout()                                        #the input textbox and the button will be here
+layout_top.addLayout(layout_right)
 
 input_combo = QComboBox()                                           #instantiating a combo box (a dropdown)
 currencies = ['USD', 'EUR', 'INR', 'JPY']
-input_combo.addItems(currencies)                                  #adding the list of currencies to the combo box
-layout.addWidget(input_combo)
+input_combo.addItems(currencies)                                    #adding the list of currencies to the combo box
+layout_left.addWidget(input_combo)
 
 target_combo = QComboBox()                                          
 target_combo.addItems(currencies)                               
-layout.addWidget(target_combo)
+layout_left.addWidget(target_combo)
 
-
-textbox = QLineEdit()                                               
-layout.addWidget(textbox)
+input_textbox = QLineEdit()                                               
+layout_right.addWidget(input_textbox, alignment=Qt.AlignmentFlag.AlignBottom)#add the textbox to the layout and then set how we want to align it using QT
 
 btn = QPushButton('Convert')
-layout.addWidget(btn)
+layout_right.addWidget(btn, alignment=Qt.AlignmentFlag.AlignBottom) 
 
 btn.clicked.connect(convert)                                        #PyQT is made of widgets, signals, and slots. the button is our widget, 'clicked' is our signal, and slot is the 'convert' function. so when our button is clicked, make_sentence function gets connected. notice we only mention the function within the parenthesis and do not actually call it
 
-output_label = QLabel('')                                          #when the application first runs, we do not want anything to appear in the label so we just give it an empty string
-layout.addWidget(output_label)
-
 window.setLayout(layout)
-
 
 window.show()                                                       #without this we cannot see the window when the app runs
 app.exec()                                                          #execute (run) the app
