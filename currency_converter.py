63ea30627de6c867b469f77f3ed3868cb3a7ce41
haydenMcKinney777 <hemc7@sbcglobal.net>
2025-01-04 13:04:00 -0600

    starter code

--- currency_converter.py
+++ currency_converter.py
@@ -1,8 +1,5 @@
 """
-Creating a GUI that will allow us to permanently delete certain files
-
-docs: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QFileDialog.html
-
+Creating a GUI that will provide the user with a definition of a word provided by the user
 """
 
 from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QHBoxLayout,
@@ -10,40 +7,24 @@ from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QHBoxLayout,
 from PySide6.QtCore import Qt
 from pathlib import Path
 
-def open_files():
-    global filenames
-    filenames, _ = QFileDialog.getOpenFileNames(window, "Select Files to be Deleted")        #QFileDialog is what enables a user to traverse the file system in order to select files in a directory. Once a user selects some files to be deleted, their absolute paths will be stored as a list held in our variable called 'filenames'
-    result_message.setText('\n'.join(filenames))                                             #the message will be the paths separated by a newline. This is what will display after the user destroys the files
-
-
-def destroy_files():
-    for filename in filenames:
-        path = Path(filename)
-        with open(path, 'wb') as file:
-            file.write(b'')                                                                 #overwrite the file
-        path.unlink()                                                                       #.unlink() will delete the path
-    result_message.setText('Destruction Successful!')
+def search_word():
+    print("Hi")
 
 app = QApplication([])                                              
 window = QWidget()                                                  
-window.setWindowTitle('File Destroyer')
-
+window.setWindowTitle('Dictionary')
 layout = QVBoxLayout()                                              
 
-instruction_text = QLabel('Select the files you want to destroy. The files will be <font color="red">permanently</font> deleted.')
+instruction_text = QLabel("Enter a word for its definition")
 layout.addWidget(instruction_text)
 
-open_button = QPushButton('Open Files')
-layout.addWidget(open_button, alignment=Qt.AlignmentFlag.AlignCenter)
-open_button.clicked.connect(open_files)
+textbox = QLineEdit()
+layout.addWidget(textbox)
 
-destroy_button = QPushButton('Destroy Files')
-layout.addWidget(destroy_button, alignment=Qt.AlignmentFlag.AlignCenter)
-destroy_button.clicked.connect(destroy_files)
-
-result_message = QLabel('')
-layout.addWidget(result_message, alignment=Qt.AlignmentFlag.AlignCenter)
+search_button = QPushButton("Search")
+layout.addWidget(search_button)
+search_button.clicked.connect(search_word)
 
 window.setLayout(layout)
-window.show()                                                       #without this we cannot see the window when the app runs
+window.show()                                                       
 app.exec()                                                          
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-open_button = QPushButton()
-
-# destroy_button = QPushButton()
-
-
-# #if file deletion successful, message = "Success." Else message = "Fail" 
-# result_message = QLabel(message)
+ 
