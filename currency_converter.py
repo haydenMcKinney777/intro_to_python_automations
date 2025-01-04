0bf373ac179c922bab67fbf07cff9de1ae4e3b33
haydenMcKinney777 <hemc7@sbcglobal.net>
2025-01-04 13:30:47 -0600

    the program can now search the json file for a word

--- currency_converter.py
+++ currency_converter.py
@@ -6,24 +6,31 @@ from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QHBoxLayout,
                                QVBoxLayout, QWidget, QLineEdit, QComboBox, QFileDialog)
 from PySide6.QtCore import Qt
 from pathlib import Path
+import json
 
-def search_word():
-    print("Hi")
+def search_word_def():
+    user_word = textbox.text()
+    resulting_def = data[user_word]
 
 app = QApplication([])                                              
 window = QWidget()                                                  
 window.setWindowTitle('Dictionary')
-layout = QVBoxLayout()                                              
+layout = QHBoxLayout()                                              
 
-instruction_text = QLabel("Enter a word for its definition")
+file_json = open("./data.json")
+data = json.load(file_json)
+
+instruction_text = QLabel("Enter a word for its definition:")
 layout.addWidget(instruction_text)
 
 textbox = QLineEdit()
 layout.addWidget(textbox)
 
+resulting_def = QLabel('')
+
 search_button = QPushButton("Search")
 layout.addWidget(search_button)
-search_button.clicked.connect(search_word)
+search_button.clicked.connect(search_word_def)
 
 window.setLayout(layout)
 window.show()                                                       
