f52caa0d3b928b68c625fcfc61bd990b834c3abb
haydenMcKinney777 <hemc7@sbcglobal.net>
2025-01-04 11:56:13 -0600

    implemented code outline and open file button

--- currency_converter.py
+++ currency_converter.py
@@ -1,70 +1,38 @@
 """
-Creating a currency converter GUI by scraping the currency exchange rate using beautifulsoup
-and the gui with PyQT
+Creating a GUI that will allow us to permanently delete certain files
 
-In this branch, we are giving the GUI a dropdown menu for the user to pick which currencies they would like to convert between
+docs: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QFileDialog.html
+
 """
 
 from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QHBoxLayout,
-                               QVBoxLayout, QWidget, QLineEdit, QComboBox)
+                               QVBoxLayout, QWidget, QLineEdit, QComboBox, QFileDialog)
 from PySide6.QtCore import Qt
-from bs4 import BeautifulSoup
-import requests
 
-def get_exchange_rates(input_currency='USD', output_currency='EUR'):
-    url = f'https://www.x-rates.com/calculator/?from={input_currency}&to={output_currency}&amount=1'
-    content = requests.get(url).text
-    soup = BeautifulSoup(content, 'html.parser')
-    rate = soup.find("span", class_="ccOutputRslt").getText()
-    rate = float(rate[:-4])
+def open_files():
+    filenames, _ = QFileDialog.getOpenFileName(window, "Select Files to be Deleted")        #QFileDialog is what enables a user to traverse the file system in order to select files in a directory
+    print(filenames)
 
-    return rate
 
-def convert():
-    input_text = input_textbox.text()                                     #we store the text that the user provided in the textbox to a variable called input_text
-    input_currency = input_combo.currentText()                      #we set the input currency to whatever the user selected in the first dropdown
-    target_currency = target_combo.currentText()
-    rate = get_exchange_rates(input_currency, target_currency)
-    output = round(float(input_text) * rate, 3)               
-    message = f"{input_text} {input_currency} is equivalent to {output} {target_currency}."
-    output_label.setText(str(message))                               #output must be converted to a string to display itself          
+app = QApplication([])                                              
+window = QWidget()                                                  
+window.setWindowTitle('File Destroyer')
 
-app = QApplication([])                                              #creating an instance of the application which takes an argument. we pass an empty list here as the argument which just means 'no argument'
-window = QWidget()                                                  #creating a window instance to hold all the elements of our application
-window.setWindowTitle('Currency Converter')
+layout = QVBoxLayout()                                              
 
-layout = QVBoxLayout()                                              #note 'QV' as in vertical. So if we add anymore widgets to this layout, they will be stacked vertically. could use QHLayout instead for horizontal
+instruction_text = QLabel('Select the files you want to destroy. The files will be <font color="red">permanently</font> deleted.')
+layout.addWidget(instruction_text)
 
-layout_top = QHBoxLayout()
-layout.addLayout(layout_top)
+open_button = QPushButton('Open Files')
+layout.addWidget(open_button, alignment=Qt.AlignmentFlag.AlignCenter)
+open_button.clicked.connect(open_files)
 
-output_label = QLabel('')                                           #when the application first runs, we do not want anything to appear in the label so we just give it an empty string
-layout.addWidget(output_label)
+destroy_button = QPushButton('Destroy Files')
+layout.addWidget(destroy_button)
 
-layout_left = QVBoxLayout()                                         #the droptown buttons will be here
-layout_top.addLayout(layout_left)
-
-layout_right = QVBoxLayout()                                        #the input textbox and the button will be here
-layout_top.addLayout(layout_right)
-
-input_combo = QComboBox()                                           #instantiating a combo box (a dropdown)
-currencies = ['USD', 'EUR', 'INR', 'JPY']
-input_combo.addItems(currencies)                                    #adding the list of currencies to the combo box
-layout_left.addWidget(input_combo)
-
-target_combo = QComboBox()                                          
-target_combo.addItems(currencies)                               
-layout_left.addWidget(target_combo)
-
-input_textbox = QLineEdit()                                               
-layout_right.addWidget(input_textbox, alignment=Qt.AlignmentFlag.AlignBottom)#add the textbox to the layout and then set how we want to align it using QT
-
-btn = QPushButton('Convert')
-layout_right.addWidget(btn, alignment=Qt.AlignmentFlag.AlignBottom) 
-
-btn.clicked.connect(convert)                                        #PyQT is made of widgets, signals, and slots. the button is our widget, 'clicked' is our signal, and slot is the 'convert' function. so when our button is clicked, make_sentence function gets connected. notice we only mention the function within the parenthesis and do not actually call it
+# #if file deletion successful, message = "Success." Else message = "Fail" 
+# result_message = QLabel(message)
 
 window.setLayout(layout)
-
 window.show()                                                       #without this we cannot see the window when the app runs
-app.exec()                                                          #execute (run) the app
+app.exec()                                                          
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+open_button = QPushButton()
+
+# destroy_button = QPushButton()
+
+
+# #if file deletion successful, message = "Success." Else message = "Fail" 
+# result_message = QLabel(message)
