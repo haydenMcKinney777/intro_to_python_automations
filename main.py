690ed203855c27b3d2aa47afb1221349a695c79a
haydenMcKinney777 <hemc7@sbcglobal.net>
2025-01-07 13:12:49 -0600

    final code

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 """
-converting SQL database to excel/csv file
+converting a SQL database into a PDF file. for further help with fpdf, visit section 7
 """
 
 import sqlite3
-import pandas
+from fpdf import FPDF
 
-con = sqlite3.connect("./database.db")      
+con = sqlite3.connect("./database.db")
 cur = con.cursor()
 
-#we don't execute the query through cur as we did previously, but through pandas instead
-df = pandas.read_sql_query("SELECT * FROM 'ips' ORDER BY asn", con)         #when using pandas to work with SQL, we need to specify both the query and the connection. 
-print(df)
+#we want to iterate over each row in our database and create a cell for each element within that row
+cur.execute('PRAGMA table_info(ips)')                                               #this command gives us info about the ips table columns
+columns = cur.fetchall()                                                            #recall fetchall fetches all rows and returns a list of tuples, but because of the line above, we get the columns instead
 
-df.to_csv('database.csv', index=None)                                                               #converting our dataframe into a csv file is as simple as calling .to_csv and providing the file path that we want the csv file to be stored. index=None simply removes the indexing the pandas automatically places on each row.
-df.to_excel('database.xlsx', index=None)                                                            #the only thing we need for this line to work is making sure we have 'openpyxl' installed i.e. pip install openpyxl
+pdf = FPDF(orientation='P', unit='pt', format='A4')                                 #create pdf instance with portrait orientation
+pdf.add_page()
+
+for column in columns:
+    pdf.set_font(family='Times', style='B', size=14)
+    pdf.cell(w=100, h=30, txt=column[1], border=1)
+
+pdf.ln()                                                                            #line break 
+
+cur.execute('SELECT * FROM "ips"')                                                  #select all rows from database
+rows = cur.fetchall()
+
+for row in rows:
+    for element in row:
+        pdf.set_font(family="Times", size=14)
+        pdf.cell(w=100, h=30, txt=str(element), border=1)                           #we need to ensure that every element within the rows are strings (since for example, asn is included in the row and it is an integer)
+    pdf.ln()
+
+pdf.output('output.pdf')
